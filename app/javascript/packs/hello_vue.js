771e18c633d528ed0109e54d1b359a357327c25f
yoshis777 <alt0prom@gmail.com>
2019-05-08 04:49:17 +0900

    vueファイル変更時、自動でリロードするように対応

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -66,7 +66,8 @@ var demo = new Vue({
             { name: 'Chuck Norris', power: Infinity },
             { name: 'Bruce Lee', power: 9000 },
             { name: 'Jackie Chan', power: 7000 },
-            { name: 'Jet Li', power: 8000 }
+            { name: 'Jet Li', power: 8000 },
+            { name: 'Exam Ple', power: 1000 }
         ]
     }
 })
